6dee4a8e438b3cb6027ec072a1780b69a83d8c93
Farzad Ghaznavi <ghfarzad@gmail.com>
2019-01-27 00:18:11 +0100

    Added partition function

--- main.cpp
+++ main.cpp
@@ -2,12 +2,42 @@
 
 using namespace std;
 
-//int partition(int* arr_ptr, int arr_sz) {
-//    // return pivot index;
-//    if (arr_sz = 1) {
-//        return 0;
-//    }
-//}
+void print(const string& name, int value) {
+    cout << name << ": " << value << endl;
+}
+
+#define log(expr) print(#expr, expr)
+
+int partition(int* arr_ptr, int arr_sz) {
+    if (arr_sz <= 1) {
+        return 0;
+    }
+
+    int pivot_index      = 0;
+    int pivot_value      = *(arr_ptr + pivot_index);
+    int index_to_compare = arr_sz - 1;
+    int num_comparisons  = 0;
+    while (++num_comparisons < arr_sz) {
+        log(num_comparisons);
+        log(index_to_compare);
+
+        int value_to_compare = *(arr_ptr + index_to_compare);
+        log(value_to_compare);
+        if (pivot_value > value_to_compare) {
+            *(arr_ptr + pivot_index)      = value_to_compare;
+            *(arr_ptr + index_to_compare) = *(arr_ptr + pivot_index + 1);
+            *(arr_ptr + pivot_index + 1)  = pivot_value;
+
+            ++pivot_index;
+        } else {
+            --index_to_compare;
+        }
+
+        log(pivot_index);
+    }
+
+    return pivot_index;
+}
 
 void sortWithInsertionSort(int* arr_ptr, int arr_sz) {
     if (arr_sz <= 1) {
@@ -56,14 +86,21 @@ void sortWithInsertionSort(int* arr_ptr, int arr_sz) {
 //}
 
 int main() {
-    int arr[] = { 3, 1, 2, 9, 3, 4, 8, 5, 6 };
+    int arr[] = { 3, 1 , 2, 9, 3, 4, 8, 5, 6 };
     int arr_sz = sizeof(arr) / sizeof(int);
-    sortWithInsertionSort(&arr[0], arr_sz);
 
-    for (int i = 0; i < arr_sz; ++i) {
-        cout << arr[i];
-    }
+    int pivot_index = partition(&arr[0], arr_sz);
+    int pivot_value = *(&arr[0] + pivot_index);
+    log(pivot_index);
+    log(pivot_value);
+    //log(arr_sz);
 
-    cout << endl;
+    //sortWithInsertionSort(&arr[0], arr_sz);
+
+    //for (int i = 0; i < arr_sz; ++i) {
+    //    cout << arr[i];
+    //}
+
+    //cout << endl;
     return 0;
 }
