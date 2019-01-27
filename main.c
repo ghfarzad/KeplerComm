9fe43864cb37478485fb80cbc33dc50c02557918
Farzad Ghaznavi <ghfarzad@gmail.com>
2019-01-27 22:50:02 +0100

    Imporoved logging

--- main.c
+++ main.c
@@ -1,10 +1,47 @@
 #include <stdio.h>
 
-#define log(expr) printf("%s: %d\n", #expr, expr)
-#define logFun()  printf("%s()\n", __FUNCTION__)
+const static int tab_stop = 4;
+
+int indent_level = 0;
+
+#define log(expr)                           \
+    printf("%*s"     , indent_level, "");   \
+    printf("%s: %d\n", #expr, expr)
+
+#define logArray(arr_ptr, arr_sz)           \
+    printf("{");                            \
+    for (int i = 0; i < arr_sz; ++i) {      \
+        if (i == arr_sz - 1) {              \
+            printf("%d",   *(arr_ptr + i)); \
+        } else {                            \
+            printf("%d, ", *(arr_ptr + i)); \
+        }                                   \
+    }                                       \
+    printf("}")
+
+#define logArrayExpr(expr, arr_ptr, arr_sz) \
+    printf("%*s",  indent_level, "");       \
+    printf("%s: ", #expr);                  \
+    logArray(arr_ptr, arr_sz);              \
+    printf("\n")
+
+#define logArrayArg(arr_ptr, arr_sz)        \
+    logArray(arr_ptr, arr_sz)
+
+#define logFun(arr_ptr, arr_sz)             \
+    printf("%*s", indent_level, "");        \
+    indent_level = indent_level + tab_stop; \
+    printf("%s(", __FUNCTION__);            \
+    logArrayArg(arr_ptr, arr_sz);           \
+    printf(") {\n")
+
+#define endLogFun()                         \
+    indent_level = indent_level - tab_stop; \
+    printf("%*s", indent_level, "");        \
+    printf("}\n")
 
 int partition(int* arr_ptr, int arr_sz) {
-    logFun();
+    logFun(arr_ptr, arr_sz);
 
     if (arr_sz <= 1) {
         return 0;
@@ -15,11 +52,7 @@ int partition(int* arr_ptr, int arr_sz) {
     int index_to_compare = arr_sz - 1;
     int num_comparisons  = 0;
     while (++num_comparisons < arr_sz) {
-        log(num_comparisons);
-        log(index_to_compare);
-
         int value_to_compare = *(arr_ptr + index_to_compare);
-        log(value_to_compare);
         if (pivot_value > value_to_compare) {
             *(arr_ptr + pivot_index)      = value_to_compare;
             *(arr_ptr + index_to_compare) = *(arr_ptr + pivot_index + 1);
@@ -30,14 +63,14 @@ int partition(int* arr_ptr, int arr_sz) {
             --index_to_compare;
         }
 
-        log(pivot_index);
     }
 
+    endLogFun();
     return pivot_index;
 }
 
 void sortWithInsertionSort(int* arr_ptr, int arr_sz) {
-    logFun();
+    logFun(arr_ptr, arr_sz);
 
     if (arr_sz <= 1) {
         return;
@@ -55,16 +88,18 @@ void sortWithInsertionSort(int* arr_ptr, int arr_sz) {
         *(arr_ptr + reverse_index + 1) = sort_candidate;
         ++forward_index;
     }
+    endLogFun();
 }
 
 void sortWithQuickSort(int* arr_ptr, int arr_sz) {
-    logFun();
+    logFun(arr_ptr, arr_sz);
 
     if (arr_sz <= 1) {
         return;
     }
 
     int pivot_index = partition(arr_ptr, arr_sz);
+    log(pivot_index);
 
     int left_partition_sz  = pivot_index;
     int right_partition_sz = arr_sz - left_partition_sz - 1;
@@ -80,40 +115,27 @@ void sortWithQuickSort(int* arr_ptr, int arr_sz) {
     } else {
         sortWithInsertionSort(arr_ptr, left_partition_sz);
     }
+    endLogFun();
 }
 
 void sort(int* arr_ptr, int arr_sz) {
-    logFun();
-
-    log(arr_sz);
+    logFun(arr_ptr, arr_sz);
 
     if (arr_sz > 10) {
         sortWithQuickSort(arr_ptr, arr_sz);
     } else {
         sortWithInsertionSort(arr_ptr, arr_sz);
     }
+    endLogFun();
 }
 
 int main() {
     int arr[]  = { 25, 102, 953, 705, 284, 999, -1, 57, 492, 33, 15, 11, 148, 998, 775 };
     int arr_sz = sizeof(arr) / sizeof(int);
 
-    //int pivot_index = partition(&arr[0], arr_sz);
-    //int pivot_value = *(&arr[0] + pivot_index);
-    //log(pivot_index);
-    //log(pivot_value);
-
     sort(&arr[0], arr_sz);
 
-    for (int i = 0; i < arr_sz; ++i) {
-        if (i == arr_sz - 1) {
-            printf("%d", arr[i]);
-        } else {
-            printf("%d-", arr[i]);
-        }
-    }
-
-    printf("\n");
+    logArrayExpr(arr, &arr[0], arr_sz);
 
     return 0;
 }
